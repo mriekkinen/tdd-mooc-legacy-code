e2b3a1484d84101a06d4f3569dcc8bc7c7a2f299
mriekkinen <matti.riekkinen@icloud.com>
2026-04-20 15:21:57 +0300

    P2: Refactoring, p4

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -37,23 +37,23 @@ export class Shop {
           }
         }
       }
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+      if (item.name != "Sulfuras, Hand of Ragnaros") {
+        item.sellIn--;
       }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                this.items[i].quality = this.items[i].quality - 1;
+      if (item.sellIn < 0) {
+        if (item.name != "Aged Brie") {
+          if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+            if (item.quality > 0) {
+              if (item.name != "Sulfuras, Hand of Ragnaros") {
+                item.quality--;
               }
             }
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
+            item.quality = 0;
           }
         } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
+          if (item.quality < 50) {
+            item.quality++;
           }
         }
       }
