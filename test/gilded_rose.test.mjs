8cd600eef927333169c1b902996dd80e27fda856
mriekkinen <matti.riekkinen@icloud.com>
2026-04-20 13:23:27 +0300

    P1: 100% mutation test coverage

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -3,6 +3,12 @@ import { expect } from "chai";
 import { Item, Shop } from "../src/gilded_rose.mjs";
 
 describe("Gilded Rose", () => {
+  test("Empty shop", () => {
+    const gildedRose = new Shop();
+    const items = gildedRose.updateQuality();
+    expect(items).to.be.empty;
+  });
+
   test("(foo, 0, 0)", () => {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
@@ -39,11 +45,69 @@ describe("Gilded Rose", () => {
     expect(items[0].quality).to.equal(3);
   });
 
+  test("(backstage passes, 11, 0)", () => {
+    const gildedRose = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 11, 0)
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[0].sellIn).to.equal(10);
+    expect(items[0].quality).to.equal(1);
+  });
+
+  test("(backstage passes, 10, 50)", () => {
+    const gildedRose = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 50)
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[0].sellIn).to.equal(9);
+    expect(items[0].quality).to.equal(50);
+  });
+
+  test("(backstage passes, 5, 49)", () => {
+    const gildedRose = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[0].sellIn).to.equal(4);
+    expect(items[0].quality).to.equal(50);
+  });
+
+  test("(backstage passes, 6, 0)", () => {
+    const gildedRose = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 6, 0)
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[0].sellIn).to.equal(5);
+    expect(items[0].quality).to.equal(2);
+  });
+
   test("(aged brie, 0, 0)", () => {
     const gildedRose = new Shop([new Item("Aged Brie", 0, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("Aged Brie");
     expect(items[0].sellIn).to.equal(-1);
     expect(items[0].quality).to.equal(2);
+  });
+
+  test("(aged brie, 0, 50)", () => {
+    const gildedRose = new Shop([new Item("Aged Brie", 0, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal("Aged Brie");
+    expect(items[0].sellIn).to.equal(-1);
+    expect(items[0].quality).to.equal(50);
+  });
+
+  test("(sulfuras, -1, 1)", () => {
+    const gildedRose = new Shop([
+      new Item("Sulfuras, Hand of Ragnaros", -1, 1)
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal("Sulfuras, Hand of Ragnaros");
+    expect(items[0].sellIn).to.equal(-1);
+    expect(items[0].quality).to.equal(1);
   });
 });
